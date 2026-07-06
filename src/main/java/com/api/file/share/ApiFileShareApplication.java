1813d1af328e1c0f2892314df192013ec5d75b2a
matigfv <matigfv@gmail.com>
2026-07-06 18:15:39 +0200

    initial

---
+++ src/main/java/com/api/file/share/ApiFileShareApplication.java
@@ -0,0 +1,13 @@
+package com.api.file.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFileShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFileShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/file/share/ApiFileShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.file.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFileShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
