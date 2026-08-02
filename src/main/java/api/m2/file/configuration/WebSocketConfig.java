4d4ca22878c99cb4ffb4f313d612a6bbdf5ef053
matigfv <matigfv@gmail.com>
2026-08-02 17:40:44 +0200

    deployment CORS WSS

--- src/main/java/api/m2/file/configuration/WebSocketConfig.java
+++ src/main/java/api/m2/file/configuration/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:8081", "https://file.eva-core.com")
+                .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:8081", "https://keep.eva-core.com")
                 .withSockJS();
     }
 }
